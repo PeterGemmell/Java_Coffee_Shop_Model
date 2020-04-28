62b8ca4b4b3d6e02c32a56d39849ac3da4cd46c6
Peter Gemmell <petergemmell65@hotmail.com>
2020-04-28 09:52:20 +0100

    cake class and cake test ran and passed

--- src/main/java/Snacks/Cake.java
+++ src/main/java/Snacks/Cake.java
@@ -1,4 +1,21 @@
 package Snacks;
 
-public class Cake {
+import Behaviours.ISell;
+
+public class Cake extends Snack implements ISell {
+
+    private String topping;
+
+    public Cake(String name, String type, String madeFrom, double costPrice, double retailPrice, String topping){
+        super(name, type, madeFrom, costPrice, retailPrice);
+        this.topping = topping;
+    }
+
+    public String getTopping() {
+        return topping;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
 }
--- src/test/java/Snacks/CakeTest.java
+++ src/test/java/Snacks/CakeTest.java
@@ -1,4 +1,43 @@
 package Snacks;
 
+import Behaviours.ISell;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CakeTest {
+
+    Cake cheeseCake;
+    ISell markup;
+
+    @Before
+    public void before(){
+        cheeseCake = new Cake("Cheesecake", "Sweet", "Cream and Biscuit", 1.00, 4.00, "Strawberries");
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("Cheesecake", cheeseCake.getName());
+    }
+
+    @Test
+    public void cheeseCakeHasTopping(){
+        assertEquals("Strawberries", cheeseCake.getTopping());
+    }
+
+    @Test
+    public void cheeseCakeHasCostPrice(){
+        assertEquals(1.00, cheeseCake.getCostPrice(), 0.01);
+    }
+
+    @Test
+    public void cheeseCakeHasRetailPrice(){
+        assertEquals(4.00, cheeseCake.getRetailPrice(), 0.01);
+    }
+
+    @Test
+    public void cheeseCakeMarkUpPrice(){
+        assertEquals(3.00, cheeseCake.calculateMarkup(), 0.01);
+    }
 }
