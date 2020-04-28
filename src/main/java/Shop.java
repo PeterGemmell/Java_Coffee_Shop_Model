6d89134bb2eab837b44f5708fcf7fa23acb21777
Peter Gemmell <petergemmell65@hotmail.com>
2020-04-28 10:38:06 +0100

    coffee shop model complete with all tests passing

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,56 @@
+import Behaviours.ISell;
+import Drinks.Drink;
+import Snacks.Snack;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private ArrayList<ISell> drinks;
+    private ArrayList<Snack> snacks;
+
+    public Shop(){
+        snacks = new ArrayList<Snack>();
+        this.drinks = new ArrayList<ISell>();
+    }
+
+    public void addDrink(ISell drink){
+        drinks.add(drink);
+    }
+
+    public void addSnacks(Snack snacks){
+        this.snacks.add(snacks);
+    }
+
+    public int getDrinkCount(){
+        return this.drinks.size();
+    }
+
+    public int getSnackCount(){
+        return this.snacks.size();
+    }
+
+    public void removeDrink(Drink drink){
+        this.drinks.remove(drink);
+    }
+
+    public void removeSnacks(Snack snacks){
+        this.snacks.remove(snacks);
+    }
+
+    public ArrayList<ISell> getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(ArrayList<ISell> drinks) {
+        this.drinks = drinks;
+    }
+
+    public ArrayList<Snack> getSnacks() {
+        return snacks;
+    }
+
+    public void setSnacks(ArrayList<Snack> snacks) {
+        this.snacks = snacks;
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,52 @@
+import Drinks.Coffee;
+import Drinks.Tea;
+import Snacks.Sandwich;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+    Coffee cappuccino;
+    Sandwich hamSalad;
+    Tea earlGrey;
+
+    @Before
+    public void setUp() throws Exception{
+        shop = new Shop();
+        cappuccino = new Coffee("Cappuccino", "Italy", "Bold", 0.50, 3.50, "Sumatra");
+        hamSalad = new Sandwich("Ham Salad", "Savory", "White Bread", 0.30, 4.50, "Ham, Tomato and Salad");
+        earlGrey = new Tea("Earl Grey", "China", "Fruity", 0.40, 3.00, "Black");
+        shop.addDrink(cappuccino);
+        shop.addDrink(earlGrey);
+        shop.removeDrink(earlGrey);
+        shop.addSnacks(hamSalad);
+
+    }
+
+    @Test
+    public void canAddDrinkToStock(){
+        shop.addDrink(cappuccino);
+        assertEquals(2,shop.getDrinkCount());
+    }
+
+    @Test
+    public void canAddSnackToStock(){
+        shop.addSnacks(hamSalad);
+        assertEquals(2,shop.getSnackCount());
+    }
+
+    @Test
+    public void canRemoveDrinkFromStock(){
+        shop.removeDrink(cappuccino);
+        assertEquals(0,shop.getDrinkCount());
+    }
+
+    @Test
+    public void canRemoveSnackFromStock(){
+        shop.removeSnacks(hamSalad);
+        assertEquals(0, shop.getSnackCount());
+    }
 }
